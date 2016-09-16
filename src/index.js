66ad2f5c900787a6335772b6545a37ba39d6dba2
maurogestoso <maurogestoso@gmail.com>
2016-09-16 14:53:36 +0100

    Implement automatic signin

--- src/index.js
+++ src/index.js
@@ -15,10 +15,17 @@ import Signout from './components/auth/Signout';
 import Feature from './components/feature';
 import reducers from './reducers';
 
+import {AUTH_USER} from './actions/types';
+
 const createStoreWithMiddleware = applyMiddleware(reduxThunk, createLogger())(createStore);
+const store = createStoreWithMiddleware(reducers);
+const token = localStorage.getItem('token');
+if (token) {
+  store.dispatch({type: AUTH_USER})
+}
 
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
+  <Provider store={store}>
     <Router history={browserHistory}>
       <Route path='/' component={App}>
         <IndexRoute component={IndexPage}/>
